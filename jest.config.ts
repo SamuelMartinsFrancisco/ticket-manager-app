b182880d37b33821759a709938d773df9dc5fc24
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2026-05-01 20:40:15 -0300

    chore: adds coverage threshold to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,15 @@ import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
-  preset: 'jest-expo'
+  preset: 'jest-expo',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    }
+  }
 };
 
 export default config;
