7f1412ee54d4e10bb8b46eca28818758e03786b1
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2026-05-09 22:16:59 -0300

    chore: adjusts jest config and expo dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,31 @@
 /** @jest-config-loader ts-node */
 import type { Config } from 'jest';
 
+const sharedIgnorePatterns = [
+  '/node_modules/',
+  '/babel.config.js',
+  '/jest.setup.js',
+  '/.expo/',
+  '/.expo-shared/',
+  '/web-build/',
+  '/assets/',
+  '/.tamagui/',
+  '/ios/',
+  '/android/',
+  'constants',
+  'theme'
+]
+
 const config: Config = {
   verbose: true,
   preset: 'jest-expo',
+  rootDir: ".",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  collectCoverageFrom: ["src/**/*.(t|j)(s|sx)"],
+  coveragePathIgnorePatterns: [...sharedIgnorePatterns],
+  testPathIgnorePatterns: [...sharedIgnorePatterns],
   coverageThreshold: {
     global: {
       branches: 80,
@@ -11,7 +33,7 @@ const config: Config = {
       lines: 80,
       statements: 80,
     }
-  }
+  },
 };
 
 export default config;
